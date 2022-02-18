d5db4ed3a32abb8bab59eb579a02009abe13b105
vishal-bacancy <vishal.jariwala@bacancy.com>
2022-02-18 16:08:29 +0530

    initial commit

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def welcome
+    
+  end
+end
