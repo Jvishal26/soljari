5ab3980473ff04d8ffbcd39b951b07197a1d24f9
vishal-bacancy <vishal.jariwala@bacancy.com>
2022-02-24 22:02:56 +0530

    added navbar and tilwindcss

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,10 +1,10 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*'
+    './app/views/**/*',
   ],
   theme: {
     extend: {
@@ -17,5 +17,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
-}
+  ],
+};
